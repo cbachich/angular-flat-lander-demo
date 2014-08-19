7b4558f30fbe3b46ae7761d4c74878f5478b0509
Christopher M. Bachicha <cbachich@gmail.com>
2014-08-19 09:34:33 -0600

    Add multiple products and show/hide (1.5)

--- js/app.js
+++ js/app.js
@@ -2,12 +2,21 @@
   var app = angular.module('store', []);
 
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.'
-  }
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: true,
+    },
+    {
+      name: "Pentagonal Gem",
+      price: 5.95,
+      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+      canPurchase: false
+    }
+  ]
 })();
