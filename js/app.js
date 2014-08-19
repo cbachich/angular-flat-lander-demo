93cff0fd3dae70d2553574b8d270bfab70e89f92
Christopher M. Bachicha <cbachich@gmail.com>
2014-08-19 12:44:35 -0600

    Add pictures

--- js/app.js
+++ js/app.js
@@ -8,15 +8,23 @@
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
       canPurchase: true,
+      images: [
+        'img/sanborn.png',
+        'img/colorado.png'
+      ]
     },
     {
       name: "Pentagonal Gem",
       price: 5.95,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: false
+      canPurchase: false,
+      images: [
+        'img/colorado.png',
+        'img/sanborn.png'
+      ]
     }
   ]
 })();
