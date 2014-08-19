9ad143d1f286a50ecd636ef40338f48e179754e6
Christopher M. Bachicha <cbachich@gmail.com>
2014-08-19 09:18:34 -0600

    Add a controller (lesson 1.3)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module('store', []);
+(function(){
+  var app = angular.module('store', []);
+
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.'
+  }
+})();
