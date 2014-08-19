0beb4376cde0ef600e989956b0f74b00abfb7f79
Christopher M. Bachicha <cbachich@gmail.com>
2014-08-19 13:33:23 -0600

    Add a image gallery (2.3)

--- js/app.js
+++ js/app.js
@@ -7,24 +7,44 @@
 
   var gems = [
     {
-      name: 'Dodecahedron',
-      price: 2,
-      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: true,
+      name: 'Azurite',
+      description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      shine: 8,
+      price: 110.50,
+      rarity: 7,
+      color: '#CCC',
+      faces: 14,
       images: [
-        'img/sanborn.png',
-        'img/colorado.png'
+        "img/gem-02.gif",
+        "img/gem-05.gif",
+        "img/gem-09.gif"
       ]
-    },
-    {
-      name: "Pentagonal Gem",
-      price: 5.95,
-      description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-      canPurchase: false,
+    }, {
+      name: 'Bloodstone',
+      description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      shine: 9,
+      price: 22.90,
+      rarity: 6,
+      color: '#EEE',
+      faces: 12,
       images: [
-        'img/colorado.png',
-        'img/sanborn.png'
+        "img/gem-01.gif",
+        "img/gem-03.gif",
+        "img/gem-04.gif"
+      ]
+    }, {
+      name: 'Zircon',
+      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+      shine: 70,
+      price: 1100,
+      rarity: 2,
+      color: '#000',
+      faces: 6,
+      images: [
+        "img/gem-06.gif",
+        "img/gem-07.gif",
+        "img/gem-10.gif"
       ]
     }
-  ]
+  ];
 })();
