9c4f6a48790cbb732020d994338be6e0c1f2661c
Christopher M. Bachicha <cbachich@gmail.com>
2014-08-19 13:50:49 -0600

    Clean up to use panel controller (2.6)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 (function(){
   var app = angular.module('store', []);
 
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
-
   var gems = [
     {
       name: 'Azurite',
@@ -47,4 +43,21 @@
       ]
     }
   ];
+
+  app.controller('StoreController', function() {
+    this.products = gems;
+  });
+
+  app.controller('PanelController', function() {
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    }
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
 })();
