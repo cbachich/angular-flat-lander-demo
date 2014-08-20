20afea0ed8ad3b8bba32da087a6b88140d601f07
Christopher M. Bachicha <cbachich@gmail.com>
2014-08-20 09:07:29 -0600

    Add ReviewController

--- js/app.js
+++ js/app.js
@@ -79,4 +79,13 @@
     };
   });
 
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
 })();
